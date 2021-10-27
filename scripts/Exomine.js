1d9b2cc618511fcdd974c1d8f232943f848ee162
heymonicakay <hey.monicakay@gmail.com>
2021-10-27 12:35:24 -0500

    created initial app layout in Exomine module.

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,1 +1,37 @@
 // Generate HTML for application
+
+export const Exomine = () => {
+  return `
+  <div class="header flex row">
+    <h1 class="heading1 text-center flex">Exomine</h1>
+  </div>
+
+  <div class="content flex column">
+
+    <section class="top-section flex row">
+      <article class="governors container flex column">
+
+      </article>
+      <article class="colony-minerals container flex column">
+
+      </article>
+
+    </section>
+
+    <section class="bottom-section flex row">
+      <article class="facilities container flex column">
+
+      </article>
+
+      <article class="facility-minerals container flex column">
+
+      </article>
+
+      <article class="cart container flex column">
+
+      </article>
+
+    </section>
+  </div>
+  `
+}
