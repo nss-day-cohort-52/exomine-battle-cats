6fd8d67708ff111420bc422285623f4a1b595eba
heymonicakay <hey.monicakay@gmail.com>
2021-10-28 18:28:55 -0500

    adjusted layout to match wireframe more accurately

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,37 +1,47 @@
-// Generate HTML for application
+/**
+ * Responsibility: Generate application HTML based on current state
+ */
+export const Exomine = () => { // Generate app HTML based on current state
 
-export const Exomine = () => {
-  // This function returns a string of HTML
   return `
   <section class="flex column top-section">
+
     <div class="header flex row">
       <h1 class="heading1 text-center flex">Exomine</h1>
     </div>
 
-    <section class="flex row">
-      <article class="governors container flex column">
-      <h2 class="heading2 text-center flex">Select a Governor</h2>
+    <div class="flex row">
+      <section class="container flex column">
 
-      </article>
+        <article class="governors container flex column">
+          <select id="governorSelect">
+            <option value="0">Please select a Governor...</option>
+          </select>
+        </article>
+
+        <article class="facilities container flex column">
+          <select id="facilitySelect">
+            <option value="0">Please select a Facility...</option>
+          </select>
+        </article>
+
+      </section>
+
       <article class="colony-minerals container flex column">
-      <h2 class="heading2 text-center flex">Colony Minerals</h2>
+        <h2 class="heading2 text-center flex">Colony Minerals</h2>
 
       </article>
 
-    </section>
+    </div>
 
   </section>
 
   <section class="bottom-section flex row">
-    <article class="facilities container flex column">
-    <h2 class="heading2 text-center flex">Select a Facility</h2>
 
-    </article>
+      <article class="facility-minerals container flex column">
+        <h2 class="heading2 text-center flex">Minerals Available</h2>
 
-    <article class="facility-minerals container flex column">
-    <h2 class="heading2 text-center flex">Available Facility Minerals</h2>
-
-    </article>
+      </article>
 
     <article class="cart container flex column">
       <h2 class="heading2 text-center flex">Cart</h2>
